4ac7bc135f5783c79a19d73f0e57847f63c4724c
Ankit Jain <ankitjainst@gmail.com>
2017-06-26 15:06:23 +0000

    Initial Commit

---
+++ src/main/java/org/mule/modules/BaseConfig.java
@@ -0,0 +1,29 @@
+package org.mule.modules;
+
+import org.mule.api.annotations.Configurable;
+import org.mule.api.annotations.param.Default;
+
+public class BaseConfig {
+	/**
+     * Base parameter
+     */
+    @Configurable
+    @Default("BaseParam")
+    private String baseParameter;
+    
+    /**
+     * Set greeting message
+     *
+     * @param baseParameter the greeting message
+     */
+    public void setBaseParameter(String baseParameter) {
+        this.baseParameter = baseParameter;
+    }
+
+    /**
+     * Get greeting message
+     */
+    public String getBaseParameter() {
+        return this.baseParameter;
+    }
+}
