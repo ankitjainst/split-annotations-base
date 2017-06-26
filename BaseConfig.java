d1ce97dda458a531f1e14e278c19267b9ac5819d
Ankit Jain <ankitjainst@gmail.com>
2017-06-26 15:52:29 +0000

    Added another parameter

--- BaseConfig.java
+++ BaseConfig.java
@@ -26,4 +26,27 @@ public class BaseConfig {
     public String getBaseParameter() {
         return this.baseParameter;
     }
+	/**
+     * Base parameter
+     */
+    @Configurable
+    @Default("BaseParam")
+    private String baseParameter1;
+    
+    /**
+     * Set greeting message
+     *
+     * @param baseParameter the greeting message
+     */
+    public void setBaseParameter1(String baseParameter1) {
+        this.baseParameter1 = baseParameter1;
+    }
+
+    /**
+     * Get greeting message
+     */
+    public String getBaseParameter1() {
+        return this.baseParameter1;
+    }
+
 }
